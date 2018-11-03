sous-chefs/chef-ruby-lvm-attrib

commit efb892b2bc3a94d16e0135606444f64fb2c01847

Added attributes 2.02.178(2) to 2.02.182(2)
Signed-off-by: Jakob Pfeiffer <pgp-jkp@pfeiffer.ws>

update-lvm.sh
@@ -41,13 +41,10 @@ msg() {
 clone_lvm2() {
 	if [ ! -d $GIT_DIR ]; then
 		msg "Checkout $repo_url"
-		install -d $GIT_DIR
-		git init
-		git remote add origin $repo_url
-		git fetch --depth 1 origin $refs --tags
+		git submodule update --init --recursive
 	else
 		msg "Update $repo_url"
-		git fetch origin $refs --tags
+		git submodule update --recursive
 	fi
 }
 

.gitmodules
@@ -1,3 +1,3 @@
 [submodule "lvm2"]
 	path = lvm2
-	url = git://git.fedorahosted.org/git/lvm2.git
+	url = git://sourceware.org/git/lvm2.git

bin/generate_field_data
@@ -96,7 +96,7 @@ TYPE_CONVERSION_MAP = {
 
 lvm_source = ARGV[0]
 
-version = File.readlines(lvm_source + VERSION_FILE)[0].split("-git ")[0]
+version = File.readlines(lvm_source + VERSION_FILE)[0].split(/-git|\s+/)[0]
 
 lvs     = []
 lvssegs = []

lib/lvm/attributes/2.02.178(2)-rc1/lvs.yaml
@@ -1,0 +1,328 @@
+# These are column to object attribute mappings
+# generated by ./bin/generate_field_data based on
+# lvm2/lib/report/columns.h
+---
+- :method: cache_dirty_blocks
+  :column: cache_dirty_blocks
+  :type_hint: Integer
+  :description: Dirty cache blocks.
+- :method: cache_read_hits
+  :column: cache_read_hits
+  :type_hint: Integer
+  :description: Cache read hits.
+- :method: cache_read_misses
+  :column: cache_read_misses
+  :type_hint: Integer
+  :description: Cache read misses.
+- :method: cache_total_blocks
+  :column: cache_total_blocks
+  :type_hint: Integer
+  :description: Total cache blocks.
+- :method: cache_used_blocks
+  :column: cache_used_blocks
+  :type_hint: Integer
+  :description: Used cache blocks.
+- :method: cache_write_hits
+  :column: cache_write_hits
+  :type_hint: Integer
+  :description: Cache write hits.
+- :method: cache_write_misses
+  :column: cache_write_misses
+  :type_hint: Integer
+  :description: Cache write misses.
+- :method: convert_lv
+  :column: convert_lv
+  :type_hint: String
+  :description: For lvconvert
+- :method: convert_lv_uuid
+  :column: convert_lv_uuid
+  :type_hint: String
+  :description: For lvconvert
+- :method: copy_percent
+  :column: copy_percent
+  :type_hint: String
+  :description: For Cache
+- :method: data_lv
+  :column: data_lv
+  :type_hint: String
+  :description: For thin and cache pools
+- :method: data_lv_uuid
+  :column: data_lv_uuid
+  :type_hint: String
+  :description: For thin and cache pools
+- :method: data_percent
+  :column: data_percent
+  :type_hint: String
+  :description: For snapshot
+- :method: kernel_cache_policy
+  :column: kernel_cache_policy
+  :type_hint: String
+  :description: Cache policy used in kernel.
+- :method: kernel_cache_settings
+  :column: kernel_cache_settings
+  :type_hint: String
+  :description: Cache settings/parameters as set in kernel
+- :method: kernel_discards
+  :column: kernel_discards
+  :type_hint: String
+  :description: For thin pools
+- :method: kernel_metadata_format
+  :column: kernel_metadata_format
+  :type_hint: Integer
+  :description: Cache metadata format used in kernel.
+- :method: active
+  :column: lv_active
+  :type_hint: String
+  :description: Active state of the LV.
+- :method: active_exclusively
+  :column: lv_active_exclusively
+  :type_hint: String
+  :description: Set if the LV is active exclusively.
+- :method: active_locally
+  :column: lv_active_locally
+  :type_hint: String
+  :description: Set if the LV is active locally.
+- :method: active_remotely
+  :column: lv_active_remotely
+  :type_hint: String
+  :description: Set if the LV is active remotely.
+- :method: allocation_locked
+  :column: lv_allocation_locked
+  :type_hint: String
+  :description: Set if LV is locked against allocation changes.
+- :method: allocation_policy
+  :column: lv_allocation_policy
+  :type_hint: String
+  :description: LV allocation policy.
+- :method: ancestors
+  :column: lv_ancestors
+  :type_hint: String
+  :description: LV ancestors ignoring any stored history of the ancestry chain.
+- :method: attr
+  :column: lv_attr
+  :type_hint: String
+  :description: Various attributes - see man page.
+- :method: check_needed
+  :column: lv_check_needed
+  :type_hint: String
+  :description: For thin pools and cache volumes
+- :method: converting
+  :column: lv_converting
+  :type_hint: String
+  :description: Set if LV is being converted.
+- :method: descendants
+  :column: lv_descendants
+  :type_hint: String
+  :description: LV descendants ignoring any stored history of the ancestry chain.
+- :method: dm_path
+  :column: lv_dm_path
+  :type_hint: String
+  :description: Internal device-mapper pathname for LV (in /dev/mapper directory).
+- :method: fixed_minor
+  :column: lv_fixed_minor
+  :type_hint: String
+  :description: Set if LV has fixed minor number assigned.
+- :method: full_ancestors
+  :column: lv_full_ancestors
+  :type_hint: String
+  :description: LV ancestors including stored history of the ancestry chain.
+- :method: full_descendants
+  :column: lv_full_descendants
+  :type_hint: String
+  :description: LV descendants including stored history of the ancestry chain.
+- :method: full_name
+  :column: lv_full_name
+  :type_hint: String
+  :description: Full name of LV including its VG
+- :method: health_status
+  :column: lv_health_status
+  :type_hint: String
+  :description: LV health status.
+- :method: historical
+  :column: lv_historical
+  :type_hint: String
+  :description: Set if the LV is historical.
+- :method: host
+  :column: lv_host
+  :type_hint: String
+  :description: Creation host of the LV
+- :method: image_synced
+  :column: lv_image_synced
+  :type_hint: String
+  :description: Set if mirror/RAID image is synchronized.
+- :method: initial_image_sync
+  :column: lv_initial_image_sync
+  :type_hint: String
+  :description: Set if mirror/RAID images underwent initial resynchronization.
+- :method: layout
+  :column: lv_layout
+  :type_hint: String
+  :description: LV layout.
+- :method: lockargs
+  :column: lv_lockargs
+  :type_hint: String
+  :description: Lock args of the LV used by lvmlockd.
+- :method: major
+  :column: lv_major
+  :type_hint: String
+  :description: Persistent major number or -1 if not persistent.
+- :method: merge_failed
+  :column: lv_merge_failed
+  :type_hint: String
+  :description: Set if snapshot merge failed.
+- :method: merging
+  :column: lv_merging
+  :type_hint: String
+  :description: Set if snapshot LV is being merged to origin.
+- :method: metadata_size
+  :column: lv_metadata_size
+  :type_hint: Integer
+  :description: For thin and cache pools
+- :method: minor
+  :column: lv_minor
+  :type_hint: String
+  :description: Persistent minor number or -1 if not persistent.
+- :method: modules
+  :column: lv_modules
+  :type_hint: String
+  :description: Kernel device-mapper modules required for this LV.
+- :method: name
+  :column: lv_name
+  :type_hint: String
+  :description: Name.  LVs created for internal use are enclosed in brackets.
+- :method: parent
+  :column: lv_parent
+  :type_hint: String
+  :description: For LVs that are components of another LV
+- :method: path
+  :column: lv_path
+  :type_hint: String
+  :description: Full pathname for LV. Blank for internal LVs.
+- :method: profile
+  :column: lv_profile
+  :type_hint: String
+  :description: Configuration profile attached to this LV.
+- :method: read_ahead
+  :column: lv_read_ahead
+  :type_hint: String
+  :description: Read ahead setting in current units.
+- :method: role
+  :column: lv_role
+  :type_hint: String
+  :description: LV role.
+- :method: size
+  :column: lv_size
+  :type_hint: Integer
+  :description: Size of LV in current units.
+- :method: skip_activation
+  :column: lv_skip_activation
+  :type_hint: String
+  :description: Set if LV is skipped on activation.
+- :method: snapshot_invalid
+  :column: lv_snapshot_invalid
+  :type_hint: String
+  :description: Set if snapshot LV is invalid.
+- :method: tags
+  :column: lv_tags
+  :type_hint: String
+  :description: Tags
+- :method: time
+  :column: lv_time
+  :type_hint: String
+  :description: Creation time of the LV
+- :method: time_removed
+  :column: lv_time_removed
+  :type_hint: String
+  :description: Removal time of the LV
+- :method: uuid
+  :column: lv_uuid
+  :type_hint: String
+  :description: Unique identifier.
+- :method: when_full
+  :column: lv_when_full
+  :type_hint: String
+  :description: For thin pools
+- :method: metadata_lv
+  :column: metadata_lv
+  :type_hint: String
+  :description: For thin and cache pools
+- :method: metadata_lv_uuid
+  :column: metadata_lv_uuid
+  :type_hint: String
+  :description: For thin and cache pools
+- :method: metadata_percent
+  :column: metadata_percent
+  :type_hint: String
+  :description: For cache and thin pools
+- :method: mirror_log
+  :column: mirror_log
+  :type_hint: String
+  :description: For mirrors
+- :method: mirror_log_uuid
+  :column: mirror_log_uuid
+  :type_hint: String
+  :description: For mirrors
+- :method: move_pv
+  :column: move_pv
+  :type_hint: String
+  :description: For pvmove
+- :method: move_pv_uuid
+  :column: move_pv_uuid
+  :type_hint: String
+  :description: For pvmove
+- :method: origin
+  :column: origin
+  :type_hint: String
+  :description: For snapshots and thins
+- :method: origin_size
+  :column: origin_size
+  :type_hint: Integer
+  :description: For snapshots
+- :method: origin_uuid
+  :column: origin_uuid
+  :type_hint: String
+  :description: For snapshots and thins
+- :method: pool_lv
+  :column: pool_lv
+  :type_hint: String
+  :description: For thin volumes
+- :method: pool_lv_uuid
+  :column: pool_lv_uuid
+  :type_hint: String
+  :description: For thin volumes
+- :method: raid_max_recovery_rate
+  :column: raid_max_recovery_rate
+  :type_hint: Integer
+  :description: For RAID1
+- :method: raid_min_recovery_rate
+  :column: raid_min_recovery_rate
+  :type_hint: Integer
+  :description: For RAID1
+- :method: raid_mismatch_count
+  :column: raid_mismatch_count
+  :type_hint: Integer
+  :description: For RAID
+- :method: raid_sync_action
+  :column: raid_sync_action
+  :type_hint: String
+  :description: For RAID
+- :method: raid_write_behind
+  :column: raid_write_behind
+  :type_hint: Integer
+  :description: For RAID1
+- :method: seg_count
+  :column: seg_count
+  :type_hint: Integer
+  :description: Number of segments in LV.
+- :method: snap_percent
+  :column: snap_percent
+  :type_hint: String
+  :description: For snapshots
+- :method: sync_percent
+  :column: sync_percent
+  :type_hint: String
+  :description: For Cache
+- :method: vg_uuid
+  :column: vg_uuid
+  :type_hint: String
+  :description: For VolumeGroup to LogicalVolume relationship.

lib/lvm/attributes/2.02.178(2)-rc1/lvsseg.yaml
@@ -1,0 +1,138 @@
+# These are column to object attribute mappings
+# generated by ./bin/generate_field_data based on
+# lvm2/lib/report/columns.h
+---
+- :method: cache_metadata_format
+  :column: cache_metadata_format
+  :type_hint: Integer
+  :description: For cache
+- :method: cache_mode
+  :column: cache_mode
+  :type_hint: String
+  :description: For cache
+- :method: cache_policy
+  :column: cache_policy
+  :type_hint: String
+  :description: The cache policy (cached segments only).
+- :method: cache_settings
+  :column: cache_settings
+  :type_hint: String
+  :description: Cache settings/parameters (cached segments only).
+- :method: chunk_size
+  :column: chunk_size
+  :type_hint: Integer
+  :description: For snapshots
+- :method: data_copies
+  :column: data_copies
+  :type_hint: Integer
+  :description: Number of data copies.
+- :method: data_offset
+  :column: data_offset
+  :type_hint: Integer
+  :description: Data offset on each image device.
+- :method: data_stripes
+  :column: data_stripes
+  :type_hint: Integer
+  :description: Number of data stripes or mirror/raid1 legs.
+- :method: devices
+  :column: devices
+  :type_hint: String
+  :description: Underlying devices used with starting extent numbers.
+- :method: discards
+  :column: discards
+  :type_hint: String
+  :description: For thin pools
+- :method: lv_uuid
+  :column: lv_uuid
+  :type_hint: String
+  :description: For LogicalVolume to LogicalVolumeSegment relationship.
+- :method: metadata_devices
+  :column: metadata_devices
+  :type_hint: String
+  :description: Underlying metadata devices used with starting extent numbers.
+- :method: new_data_offset
+  :column: new_data_offset
+  :type_hint: Integer
+  :description: New data offset after any reshape on each image device.
+- :method: parity_chunks
+  :column: parity_chunks
+  :type_hint: Integer
+  :description: Number of (rotating) parity chunks.
+- :method: region_size
+  :column: region_size
+  :type_hint: Integer
+  :description: For mirrors/raids
+- :method: reshape_len
+  :column: reshape_len
+  :type_hint: Integer
+  :description: Size of out-of-place reshape space in current units.
+- :method: reshape_len_le
+  :column: reshape_len_le
+  :type_hint: Integer
+  :description: Size of out-of-place reshape space in logical extents.
+- :method: le_ranges
+  :column: seg_le_ranges
+  :type_hint: String
+  :description: Ranges of Logical Extents of underlying devices in command line format.
+- :method: metadata_le_ranges
+  :column: seg_metadata_le_ranges
+  :type_hint: String
+  :description: Ranges of Logical Extents of underlying metadata devices in command
+    line format.
+- :method: monitor
+  :column: seg_monitor
+  :type_hint: String
+  :description: Dmeventd monitoring status of the segment.
+- :method: pe_ranges
+  :column: seg_pe_ranges
+  :type_hint: String
+  :description: Ranges of Physical Extents of underlying devices in command line format
+    (deprecated
+- :method: size
+  :column: seg_size
+  :type_hint: Integer
+  :description: Size of segment in current units.
+- :method: size_pe
+  :column: seg_size_pe
+  :type_hint: Integer
+  :description: Size of segment in physical extents.
+- :method: start
+  :column: seg_start
+  :type_hint: Integer
+  :description: Offset within the LV to the start of the segment in current units.
+- :method: start_pe
+  :column: seg_start_pe
+  :type_hint: Integer
+  :description: Offset within the LV to the start of the segment in physical extents.
+- :method: tags
+  :column: seg_tags
+  :type_hint: String
+  :description: Tags
+- :method: segtype
+  :column: segtype
+  :type_hint: String
+  :description: Type of LV segment.
+- :method: stripe_size
+  :column: stripe_size
+  :type_hint: Integer
+  :description: For stripes
+- :method: stripes
+  :column: stripes
+  :type_hint: Integer
+  :description: Number of stripes or mirror/raid1 legs.
+- :method: thin_count
+  :column: thin_count
+  :type_hint: Integer
+  :description: For thin pools
+- :method: thin_id
+  :column: thin_id
+  :type_hint: Integer
+  :description: For thin volume
+- :method: transaction_id
+  :column: transaction_id
+  :type_hint: Integer
+  :description: For thin pools
+- :method: zero
+  :column: zero
+  :type_hint: String
+  :description: For thin pools and volumes

lib/lvm/attributes/2.02.178(2)-rc1/pvs.yaml
@@ -1,0 +1,113 @@
+# These are column to object attribute mappings
+# generated by ./bin/generate_field_data based on
+# lvm2/lib/report/columns.h
+---
+- :method: dev_size
+  :column: dev_size
+  :type_hint: Integer
+  :description: Size of underlying device in current units.
+- :method: pe_start
+  :column: pe_start
+  :type_hint: Integer
+  :description: Offset to the start of data on the underlying device.
+- :method: allocatable
+  :column: pv_allocatable
+  :type_hint: String
+  :description: Set if this device can be used for allocation.
+- :method: attr
+  :column: pv_attr
+  :type_hint: String
+  :description: Various attributes - see man page.
+- :method: ba_size
+  :column: pv_ba_size
+  :type_hint: Integer
+  :description: Size of PV Bootloader Area in current units.
+- :method: ba_start
+  :column: pv_ba_start
+  :type_hint: Integer
+  :description: Offset to the start of PV Bootloader Area on the underlying device
+    in current units.
+- :method: duplicate
+  :column: pv_duplicate
+  :type_hint: String
+  :description: Set if PV is an unchosen duplicate.
+- :method: exported
+  :column: pv_exported
+  :type_hint: String
+  :description: Set if this device is exported.
+- :method: ext_vsn
+  :column: pv_ext_vsn
+  :type_hint: Integer
+  :description: PV header extension version.
+- :method: fmt
+  :column: pv_fmt
+  :type_hint: String
+  :description: Type of metadata.
+- :method: free
+  :column: pv_free
+  :type_hint: Integer
+  :description: Total amount of unallocated space in current units.
+- :method: in_use
+  :column: pv_in_use
+  :type_hint: String
+  :description: Set if PV is used.
+- :method: major
+  :column: pv_major
+  :type_hint: String
+  :description: Device major number.
+- :method: mda_count
+  :column: pv_mda_count
+  :type_hint: Integer
+  :description: Number of metadata areas on this device.
+- :method: mda_free
+  :column: pv_mda_free
+  :type_hint: Integer
+  :description: Free metadata area space on this device in current units.
+- :method: mda_size
+  :column: pv_mda_size
+  :type_hint: Integer
+  :description: Size of smallest metadata area on this device in current units.
+- :method: mda_used_count
+  :column: pv_mda_used_count
+  :type_hint: Integer
+  :description: Number of metadata areas in use on this device.
+- :method: minor
+  :column: pv_minor
+  :type_hint: String
+  :description: Device minor number.
+- :method: missing
+  :column: pv_missing
+  :type_hint: String
+  :description: Set if this device is missing in system.
+- :method: name
+  :column: pv_name
+  :type_hint: String
+  :description: Name.
+- :method: pe_alloc_count
+  :column: pv_pe_alloc_count
+  :type_hint: Integer
+  :description: Total number of allocated Physical Extents.
+- :method: pe_count
+  :column: pv_pe_count
+  :type_hint: Integer
+  :description: Total number of Physical Extents.
+- :method: size
+  :column: pv_size
+  :type_hint: Integer
+  :description: Size of PV in current units.
+- :method: tags
+  :column: pv_tags
+  :type_hint: String
+  :description: Tags
+- :method: used
+  :column: pv_used
+  :type_hint: Integer
+  :description: Total amount of allocated space in current units.
+- :method: uuid
+  :column: pv_uuid
+  :type_hint: String
+  :description: Unique identifier.
+- :method: vg_uuid
+  :column: vg_uuid
+  :type_hint: String
+  :description: For VolumeGroup to PhysicalVolume relationship.

lib/lvm/attributes/2.02.178(2)-rc1/pvsseg.yaml
@@ -1,0 +1,16 @@
+# These are column to object attribute mappings
+# generated by ./bin/generate_field_data based on
+# lvm2/lib/report/columns.h
+---
+- :method: pv_uuid
+  :column: pv_uuid
+  :type_hint: String
+  :description: For PhysicalVolume to PhysicalVolumeSegment relationship.
+- :method: size
+  :column: pvseg_size
+  :type_hint: Integer
+  :description: Number of extents in segment.
+- :method: start
+  :column: pvseg_start
+  :type_hint: Integer
+  :description: Physical Extent number of start of segment.

lib/lvm/attributes/2.02.178(2)-rc1/vgs.yaml
@@ -1,0 +1,136 @@
+# These are column to object attribute mappings
+# generated by ./bin/generate_field_data based on
+# lvm2/lib/report/columns.h
+---
+- :method: lv_count
+  :column: lv_count
+  :type_hint: Integer
+  :description: Number of LVs.
+- :method: max_lv
+  :column: max_lv
+  :type_hint: Integer
+  :description: Maximum number of LVs allowed in VG or 0 if unlimited.
+- :method: max_pv
+  :column: max_pv
+  :type_hint: Integer
+  :description: Maximum number of PVs allowed in VG or 0 if unlimited.
+- :method: pv_count
+  :column: pv_count
+  :type_hint: Integer
+  :description: Number of PVs in VG.
+- :method: snap_count
+  :column: snap_count
+  :type_hint: Integer
+  :description: Number of snapshots.
+- :method: allocation_policy
+  :column: vg_allocation_policy
+  :type_hint: String
+  :description: VG allocation policy.
+- :method: attr
+  :column: vg_attr
+  :type_hint: String
+  :description: Various attributes - see man page.
+- :method: clustered
+  :column: vg_clustered
+  :type_hint: String
+  :description: Set if VG is clustered.
+- :method: exported
+  :column: vg_exported
+  :type_hint: String
+  :description: Set if VG is exported.
+- :method: extendable
+  :column: vg_extendable
+  :type_hint: String
+  :description: Set if VG is extendable.
+- :method: extent_count
+  :column: vg_extent_count
+  :type_hint: Integer
+  :description: Total number of Physical Extents.
+- :method: extent_size
+  :column: vg_extent_size
+  :type_hint: Integer
+  :description: Size of Physical Extents in current units.
+- :method: fmt
+  :column: vg_fmt
+  :type_hint: String
+  :description: Type of metadata.
+- :method: free
+  :column: vg_free
+  :type_hint: Integer
+  :description: Total amount of free space in current units.
+- :method: free_count
+  :column: vg_free_count
+  :type_hint: Integer
+  :description: Total number of unallocated Physical Extents.
+- :method: lock_args
+  :column: vg_lock_args
+  :type_hint: String
+  :description: Lock args of the VG used by lvmlockd.
+- :method: lock_type
+  :column: vg_lock_type
+  :type_hint: String
+  :description: Lock type of the VG used by lvmlockd.
+- :method: mda_copies
+  :column: vg_mda_copies
+  :type_hint: Integer
+  :description: Target number of in use metadata areas in the VG.
+- :method: mda_count
+  :column: vg_mda_count
+  :type_hint: Integer
+  :description: Number of metadata areas on this VG.
+- :method: mda_free
+  :column: vg_mda_free
+  :type_hint: Integer
+  :description: Free metadata area space for this VG in current units.
+- :method: mda_size
+  :column: vg_mda_size
+  :type_hint: Integer
+  :description: Size of smallest metadata area for this VG in current units.
+- :method: mda_used_count
+  :column: vg_mda_used_count
+  :type_hint: Integer
+  :description: Number of metadata areas in use on this VG.
+- :method: missing_pv_count
+  :column: vg_missing_pv_count
+  :type_hint: Integer
+  :description: Number of PVs in VG which are missing.
+- :method: name
+  :column: vg_name
+  :type_hint: String
+  :description: Name.
+- :method: partial
+  :column: vg_partial
+  :type_hint: String
+  :description: Set if VG is partial.
+- :method: permissions
+  :column: vg_permissions
+  :type_hint: String
+  :description: VG permissions.
+- :method: profile
+  :column: vg_profile
+  :type_hint: String
+  :description: Configuration profile attached to this VG.
+- :method: seqno
+  :column: vg_seqno
+  :type_hint: Integer
+  :description: Revision number of internal metadata.  Incremented whenever it changes.
+- :method: size
+  :column: vg_size
+  :type_hint: Integer
+  :description: Total size of VG in current units.
+- :method: sysid
+  :column: vg_sysid
+  :type_hint: String
+  :description: System ID of the VG indicating which host owns it.
+- :method: systemid
+  :column: vg_systemid
+  :type_hint: String
+  :description: System ID of the VG indicating which host owns it.
+- :method: tags
+  :column: vg_tags
+  :type_hint: String
+  :description: Tags
+- :method: uuid
+  :column: vg_uuid
+  :type_hint: String
+  :description: Unique identifier.

commit e8c073f883133730fa369443087122f2be6d1cf8

update update script to actually update
reverts logic broken in efb892b

Signed-off-by: Elan Ruusamäe <glen@pld-linux.org>

update-lvm.sh
@@ -12,9 +12,6 @@
 # To add attributes:
 # * Download and extract LVM2 source version from: https://sourceware.org/git/?p=lvm2.git;a=tags
 # * Fork this repository
-# * `git clone your-forked-repo`
-# * `cd your-forked-repo`
-# * `git checkout -b mybranch`
 # * `bin/generate_field_data path/to/lvm2-source`
 #   * See missing attribute type note below if there's issues, otherwise will just return "Done."
 # * `mv LVM_VERSION_FULL lib/lvm/attributes/LVM_VERSION`
@@ -22,11 +19,8 @@
 #   * LVM_VERSION being something like 2.02.86(2) or 2.02.98(2)
 # * `git commit -am "Added LVM_VERSION attributes"`
 # * `git push origin mybranch`
-# * Submit PR to this repository. **Please make sure to point your pull at the
-#   `next` branch -- NOT MASTER!**
 #
 
-repo_url=git://sourceware.org/git/lvm2.git
 refs=refs/heads/master:refs/remotes/origin/master
 pattern=v2_02_*
 git_dir=lvm2/.git
@@ -38,14 +32,14 @@ msg() {
 }
 
 # do initial clone or update LVM2 repository
-clone_lvm2() {
-	if [ ! -d $GIT_DIR ]; then
-		msg "Checkout $repo_url"
+update_lvm2_repo() {
+	if [ ! -e lvm2/.git ]; then
+		msg "Checkout LVM2 repository"
 		git submodule update --init --recursive
-	else
-		msg "Update $repo_url"
-		git submodule update --recursive
 	fi
+
+	msg "Update LVM2 repository"
+	GIT_DIR=$git_dir git fetch origin $refs --tags
 }
 
 process_lvm2_version() {
@@ -60,9 +54,7 @@ process_lvm2_version() {
 	fi
 
 	msg "Checkout LVM2 $tag"
-	cd lvm2
-	git checkout $tag
-	cd ..
+	GIT_DIR=lvm2/.git git checkout $tag
 
 	version=$(awk '{print $1}' lvm2/VERSION)
 	msg "LVM2 Full Version: $version"
@@ -111,16 +103,12 @@ process_lvm2_version() {
 	return 0
 }
 
-
-GIT_DIR=$git_dir clone_lvm2
+update_lvm2_repo
 
 if [ "$1" = "-a" ]; then
 	# obtain all versions
-	set -- $(GIT_DIR=$git_dir git tag -l $pattern)
+	set -- $(GIT_DIR=lvm2/.git git tag -l $pattern)
 fi
-
-# it shouldn't be exported, but somewhy is. unset
-unset GIT_DIR
 
 # process versions specified on commandline,
 # otherwise iterate over all LVM2 tags
@@ -128,6 +116,9 @@ for tag in "$@"; do
 	process_lvm2_version $tag || continue
 	updated=1
 done
+
+# keep the pointer to master branch
+GIT_DIR=lvm2/.git git checkout master
 
 if [ -z "$updated" ]; then
 	echo >&2 "Nothing updated"